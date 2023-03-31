8a1611c799245651fecb20ea3b8c25a97d5a73a1
quazlee <quazlee29@gmail.com>
2023-03-31 19:07:16 -0400

    changes

--- cpu.js
+++ cpu.js
@@ -1,3 +1,24 @@
-export class Cpu{
-    
+import { RegisterCollection } from "./registerCollection.js"
+import { Memory } from "./memory.js";
+
+export class Cpu {
+    constructor() {
+        this.registers = new RegisterCollection();
+        this.currentOpcode = 0x0000;
+        this.opcodeTicks = 0;
+        this.memory = new Memory();
+
+    }
+
+    fetch() {
+        
+    }
+
+    decode(opcode) {
+
+    }
+
+    tickClock(){
+        
+    }
 }
