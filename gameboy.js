3217396cf009559c557703fcb15cc804443fabb8
quazlee <quazlee29@gmail.com>
2023-06-22 21:52:22 -0400

    a

--- gameboy.js
+++ gameboy.js
@@ -5,6 +5,7 @@ export class Gameboy {
         this.cpu = null;
         this.gpu = null;
         let currentOpcode = null;
+        let lastLoopEnd = 0;
     }
 
     initialize(romArray){
@@ -13,37 +14,20 @@ export class Gameboy {
     }
 
     mainLoop() {
+        var start = (performance.now() + performance.timeOrigin);
         let breakpoint = false;
-        var duration = 0;
-        var operations = 0;
         while(this.cpu.frameReady == false){
-            var start = (performance.now() + performance.timeOrigin);
-
             this.currentOpcode = this.cpu.decode();
             this.cpu.execute(this.currentOpcode);
-            duration += (performance.now() + performance.timeOrigin) - start;
-            operations++;
         }
         this.cpu.opcodeTicks -= 70223;
-        
+        var duration= (performance.now() + performance.timeOrigin) - start;
         this.cpu.frameReady = false;
         document.getElementById("frames-elapsed").stepUp(1);
-        console.log(duration);
-        console.log(operations);
+        console.log("A".concat(" ", duration));
+        console.log((performance.now() + performance.timeOrigin) - this.lastLoopEnd - duration);
+        this.lastLoopEnd = (performance.now() + performance.timeOrigin);
     }
-
-    // singleOpcode(){
-
-    //     this.cpu.execute();
-    // }
-
-    // breakpoint(){
-        
-    //     while(this.cpu.programCounter != breakpoint){
-    //         let [high, low] = this.decode();
-    //         this.cpu.execute([high, low]);
-    //     }
-    // }
 
     // testTile(){
     //     let tile = [0x3c, 0x7e, 0x42, 0x42, 0x42, 0x42, 0x42, 0x42, 0x7e, 0x5e, 0x7e, 0x0a, 0x7c, 0x56, 0x38, 0x7c]
