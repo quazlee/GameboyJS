eb33d354dc2dad3cbb5ef52db2e182c872d87548
quazlee <quazlee29@gmail.com>
2023-08-24 16:28:21 -0400

    a

--- controls.js
+++ controls.js
@@ -1,5 +1,25 @@
-class controls{
+export class Controls{
     constructor(){
-        
+        document.addEventListener("keydown", this.keyDownHandler, false);
+        document.addEventListener("keyup", this.keyUpHandler, false);
+
+        this.up = false;
+        this.down = false;
+        this.left = false;
+        this.right = false;
+        this.a = false;
+        this.b = false;
+        this.start = false;
+        this.select = false;
+    }
+
+    keyDownHandler(event){
+        switch(event.keycode){
+            // case 
+        }
+    }
+
+    keyUpHandler(){
+
     }
 }
